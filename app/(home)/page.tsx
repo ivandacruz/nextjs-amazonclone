c1ea4000f893f5e40cf2ae696bed50f11277fc50
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2025-01-08 21:21:39 -0300

    add home search logo menu sign in and cart

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default async function Page() {
+  return (
+    <div className=''>
+        <h1 className='h1-bold text-center p-10'>Home Page Contyent</h1>
+    </div>
+  )
+}
